db90eaf0dc93b200c8b4ef3c1eb8ad11d6cd2a61
liblaf <30631553+liblaf@users.noreply.github.com>
2025-07-11 15:18:41 +0800

    feat: add scripts for pulling and pushing profile data

---
+++ .config/mise/tasks/push-profile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+NAMESPACE_ID="$(rbw get --field "NAMESPACE_ID" "API")"
+WORKING_DIRECTORY="$(git rev-parse --show-toplevel)"
+
+function get() {
+  local key="$1"
+  wrangler kv key get "$key" --namespace-id "$NAMESPACE_ID" --remote |
+    sed "1 { /Proxy environment variables detected. We'll use your proxy for fetch requests./d }"
+}
+
+function put() {
+  local key="$1"
+  local path="$2"
+  wrangler kv key put "$key" --namespace-id "$NAMESPACE_ID" --path "$path" --remote
+}
+
+put "profile.yaml" "$WORKING_DIRECTORY/profile.yaml"
